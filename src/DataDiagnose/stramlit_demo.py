9d01167517c91fb2024d2abbcbaa53b072c4fdbf
buzh <buzh@3clear.com>
2020-11-02 18:11:09 +0800

    Feature: test streamlit

--- src/DataDiagnose/stramlit_demo.py
+++ src/DataDiagnose/stramlit_demo.py
@@ -6,3 +6,15 @@
 @version     : 1.0
 '''
 
+import os 
+import sys
+sys.path.append("../../")
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+from dao.orm.SQLite_dynamic import ObsDataRaw, ObsDataQcLinear, AutoStation
+from dao.orm.SQLite_dynamic import ForecastArima, ForecastFbprophet, ForecastLSTM, ForecastGRU
+
+
